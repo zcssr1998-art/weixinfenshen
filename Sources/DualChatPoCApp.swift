0d65ea97d41b11bb93f38a40005bdd82e49da4f6
zcssr1998-art <zcssr1998@gmail.com>
2026-09-12 01:35:23 +0800

    feat: wire diagnostics into app lifecycle

--- Sources/DualChatPoCApp.swift
+++ Sources/DualChatPoCApp.swift
@@ -3,11 +3,20 @@ import SwiftUI
 @main
 struct DualChatPoCApp: App {
     @StateObject private var store = LocalAccountStore()
+    @StateObject private var keychain = KeychainStore()
+    @StateObject private var appGroup = AppGroupDiagnostics()
+    @StateObject private var deepLink = DeepLinkStore()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(store)
+                .environmentObject(keychain)
+                .environmentObject(appGroup)
+                .environmentObject(deepLink)
+                .onOpenURL { url in
+                    deepLink.handle(url)
+                }
         }
     }
 }
